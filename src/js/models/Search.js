d9c145844078fdfd878917c9ab831619e0ba8531
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2021-09-03 17:33:06 +0430

    Add search controller.

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -9,7 +9,7 @@ export default class Search {
         try {
             const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);
             this.result = result.data.recipes;
-            console.log(this.result);
+            // console.log(this.result);
         } catch (error) {
             alert(error);
         }
