37a65360445b51105a68503e4ce360e7b9495ca9
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2021-09-19 11:29:20 +0430

    Add render loader and clear loader methods.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // Global app controller 
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 
 /**
@@ -26,11 +26,13 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchResultDiv);
 
         // 4) Search for recipes
         await state.search.getResult();
 
         // 5) Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     };
 
