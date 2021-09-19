649d62c78cc75df117cef2d0c0b5def282c69cc8
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2021-09-19 17:36:49 +0430

    Improve pagination design.

--- src/js/index.js
+++ src/js/index.js
@@ -47,6 +47,7 @@ elements.searchResultPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
     if(btn) {
         const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
         searchView.renderResults(state.search.result, goToPage);
     }
 });
